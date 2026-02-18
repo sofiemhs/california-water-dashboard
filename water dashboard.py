a6db998162b4c3b9c637e264cf39e8efc6f57718
sofiemhs <sofiemaya1@gmail.com>
2026-02-18 14:42:32 -0800

    Update water dashboard.py

--- water dashboard.py
+++ water dashboard.py
@@ -20,7 +20,7 @@ def get_base64(file):
     with open(file, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img_base64 = get_base64("background.jpg")
+img_base64 = get_base64("wildlifeheader.jpg")
 
 st.markdown(f"""
 <style>
@@ -161,6 +161,7 @@ st.markdown("""
 """)
 
 st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
